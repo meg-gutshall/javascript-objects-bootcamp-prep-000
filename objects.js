fe39f94f2782f8de4108fb1566148af524003b98
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-05-01 20:10:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist["Phil Ochs"] = "Here's to the State of Mississippi"
 }
 
+function removeFromPlaylist(playlist, artistName) {
+  
+}
