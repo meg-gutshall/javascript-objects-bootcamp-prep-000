8c29e9ca684b4ef92c8920369c692870b7c477d0
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-05-01 20:10:47 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  Alison: "Slowdive",
+  Sometimes: "My Bloody Valentine"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist["Phil Ochs"] = "Here's to the State of Mississippi"
+}
+
